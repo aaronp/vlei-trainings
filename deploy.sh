98d82695599941c90c061bf69a80bea3eb330fcd
essbante <esteban.garcia@gmail.com>
2025-06-17 14:19:22 +0000

    feat: add sally setup

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Direct mode Sally
+export WEBHOOK_HOST=http://hook:9923
+export DIRECT_SALLY_HOST=http://direct-sally:9823
+export DIRECT_SALLY=direct-sally
+export DIRECT_SALLY_ALIAS=directSally
+export DIRECT_SALLY_PASSCODE=4TBjjhmKu9oeDp49J7Xdy
+export DIRECT_SALLY_SALT=0ABVqAtad0CBkhDhCEPd514T
+export DIRECT_SALLY_PRE=ECLwKe5b33BaV20x7HZWYi_KUXgY91S41fRL2uCaf4WQ
+export GEDA_PRE=EC51WzzALD5d2PQl---qfZUVHLi3ldFLUNmHyMOjIRPC
+
 # Check if vlei network exists, create it if not
 # echo "Checking for keri_network..."
 # if ! docker network ls | grep -q keri_network; then
